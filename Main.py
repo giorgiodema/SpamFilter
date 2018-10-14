6a939d08796f9b461b1b39a26972c24ad59988a7
giorgiodema <giorgiodema@gmail.com>
2018-10-14 15:40:54 +0200

    working01

---
+++ Main.py
@@ -0,0 +1,48 @@
+
+import SpamFilter
+import functools
+import re
+
+TRAINING_SET_SIZE = 0.9
+
+# LOADING DATA FROM FILE -------------------------------------------------------------
+file = open("SMSSpamCollection","r").readlines()
+regexp = re.compile("\W*")
+
+dataset = list(map(lambda x: x[0:-1],list(map( lambda x: regexp.split(x),file))))
+for i in range(0,len(dataset)):
+    dataset[i] = list(map(lambda x:x.lower(),dataset[i]))
+
+
+# SPLITTING DATASET INTO TEST AND TRAINING SET -----------------------------------------
+trainingSet = dataset[0: int(len(dataset)*TRAINING_SET_SIZE)]
+testSet     = dataset[int(len(dataset)*TRAINING_SET_SIZE):]
+
+
+
+# LISTING TARGET VALUES AND ATTRIBUTES ----------------------------------------------
+tv = ["ham","spam"]
+attributes = []
+
+for i in range(0,len(trainingSet)):
+    attributes.extend([x for x in dataset[i] if not x in tv if not x in attributes])
+
+
+# TRAINING SPAM FILTER
+sf = SpamFilter.BayesianClassifier(tv,attributes,trainingSet)
+sf.Learn()
+
+
+
+# TESTING SPAM FILTER
+numTries = len(testSet)
+numSuccesses = 0
+
+for i in range(0,len(testSet)):
+    predictedValue = sf.Classify(testSet[i][1:])
+    print("Test #{}    | Predicted: {}    | Expected: {}".format(i,predictedValue,testSet[i][0]))
+    
+    if predictedValue == testSet[i][0]:
+        numSuccesses +=1
+
+print("\n\nSuccess percentage: {} %".format((numSuccesses/numTries)*100))
--- SpamFilter.py
+++ SpamFilter.py
@@ -1,65 +1,60 @@
+from functools import reduce
 
-"""
 
-        targetValues = [spam,ham]
         
-        attributes = [list of words appearing in all messages]
 
-        dataset = [list of documents]
 
-        document = { class:"HAM/SPAM", word1:freq1, word2:freq2, ... }
-
-        aPriori = { "SPAM":aPrioriProb, "HAM":aprioriProb }
-        likelihood = { ("SPAM":"word1"):prob, ...}
+class BayesianClassifier:
 
 
 
-
-"""
-
-
-
-class BayesianLearn:
-
-
-
-    def __init__(self, targetValues, attributes, dataset):
+    def __init__(self,tv,a,d):
         
-        self.targetValues = targetValues
-        self.attributes = attributes
-        self.dataset = dataset
+        self.targetValues = tv      # [spam,ham]
+        self.attributes = a         # [list of words appearing in all messages]
+        self.dataset = d            # ["spam/ham", "word1", "word2", ..., "wordn"]
 
-        self.priorProb = {}
-        self.likelihood = {}
+        self.priorProb = {}         # {"spam":'prior spam probability', "ham": 'prior ham probability'}
+        self.likelihood = {}        # { ("spam/ham":"word1"):prob, ...}
+
+        return
+
+
+    def estimatePriorProb(self,tv):
+        return len(list(filter(lambda x: x[0] == tv, self.dataset))) / len(self.dataset)
+
+
+    def estimateLikelihood(self,tv,a):
+        return len(list(filter(lambda x: x[0] == tv and a in x, self.dataset))) / len(list(filter(lambda x: x[0] == tv, self.dataset)))
+
+
+    def Learn(self):
+
+        for tv in self.targetValues:
+            self.priorProb[tv] = self.estimatePriorProb(tv)
+
+            for a in self.attributes:
+                self.likelihood[(tv,a)] = self.estimateLikelihood(tv,a)
+
 
         return
 
 
 
 
-    def Learn():
 
-        for tv in targetValues:
-            priorProb[tv] = estimatePriorProb(dataset,tv)
-
-            for a in attributes:
-                likelihood[(tv,a)] = estimateLikelihood(dataset,tv,a)
-
-
-        return
-
-
-
-    def Classify(document):
+    def Classify(self,document):
 
         posteriorProb = {}
 
-        for tv in targetValues:
-            posteriorProb[tv] = ## TODO
+        for tv in self.targetValues:
+            priorElem = [x[1] for x in list(self.likelihood.items()) if x[0][0]==tv if x[0][1] in document]
+            posteriorProb[tv] = 0 if len(priorElem)==0 else reduce(lambda x,y: x*y, priorElem) * self.priorProb[tv]
 
+        inversePosteriorProb =  {v: k for k, v in posteriorProb.items()}
+        max =  reduce(lambda x,y: x if(x>y) else y, inversePosteriorProb)
 
-
-
+        return inversePosteriorProb[max]
 
 
 
